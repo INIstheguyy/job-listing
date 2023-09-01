d5202c2fcce0aa4b9528cf6f19c369f81922f11f
Komoini <komolafeinioluwa9@gmail.com>
2023-09-01 08:49:20 +0100

    intial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./index.css"
+import data from './data.json'
+
+function App() {
+  console.log(data)
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
